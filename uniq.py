aba3bef406b29c71f47602d9ee64a9155c612cb8
Andrea Crotti <andrea.crotti.0@gmail.com>
2014-12-04 21:00:03 +0000

    add another option

--- uniq.py
+++ uniq.py
@@ -2,29 +2,20 @@ import sys
 import argparse
 import unittest
 
-from collections import Counter
 
-
-def count(inp):
-    pass
-
-
-def uniq(inp, with_count=False):
+def uniq(lines, with_count=False, only_repeated=False):
     counter = 1
-    last = None
+    last = lines[0]
 
-    for line in inp.splitlines() + ['\n']:
-        if last is None:
-            last = line
-            continue
-
+    for line in lines[1:] + ['\n']:
         if line != last:
             if with_count:
                 result = "{} {}".format(counter, last)
             else:
                 result = last
 
-            yield result
+            if (not only_repeated) or counter > 1:
+                yield result
             counter = 1
         else:
             counter += 1
@@ -34,19 +25,26 @@ def uniq(inp, with_count=False):
 
 class TestUniq(unittest.TestCase):
     def test_simple_uniq(self):
-        inp, out = "line\nline\ntest\nline", ["line", "test", "line"]
+        inp, out = ["line", "line", "test", "line"], ["line", "test", "line"]
         self.assertEqual(list(uniq(inp)), out)
 
     def test_counting_occurrences(self):
-        inp = "line\nline\nhello"
+        inp = ["line", "line", "hello"]
         desired = ["2 line", "1 hello"]
         self.assertEqual(list(uniq(inp, with_count=True)), desired)
+
+    def test_only_repeated(self):
+        inp = ["line", "line", "hello"]
+        desired = ["line"]
+        self.assertEqual(list(uniq(inp, only_repeated=True)), desired)
 
 
 def parser():
     parser = argparse.ArgumentParser(description='uniq')
     parser.add_argument('input_file', nargs=1)
     parser.add_argument('output_file', nargs='?')
+    parser.add_argument('-c', dest='counter', action='store_true')
+    parser.add_argument('-d', dest='only_repeated', action='store_true')
 
     return parser.parse_args()
 
@@ -57,13 +55,9 @@ if __name__ == '__main__':
     else:
         ns = parser()
         with open(ns.input_file[0]) as input:
-            result = '\n'.join(uniq(input.read())) + '\n'
+            lines = input.readlines()
+            result = ''.join(uniq(lines, with_count=ns.counter,
+                                  only_repeated=ns.only_repeated))
+
             output = open(ns.output_file, 'w') if ns.output_file else sys.stdout
             output.write(result)
-
-
-
-
-
-
-
